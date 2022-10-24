08af206c10196dc9d074d94652f46ad1e051c3ff
Max Kelly <33044453+maxckelly@users.noreply.github.com>
2022-10-24 05:04:16 -0400

    Task/test id on bottom sheet (#128)
    * Add test id to bottom sheet
    
    * Increase package version

--- src/components/editAndDeleteBottomSheet/EditAndDeleteBottomSheet.tsx
+++ src/components/editAndDeleteBottomSheet/EditAndDeleteBottomSheet.tsx
@@ -5,17 +5,26 @@ import { Box } from '../../themes/box/Box';
 
 type EditAndDeleteBottomSheetTypes = {
   isVisible: boolean;
+  deleteStoryTestID: string;
+  editStoryTestID: string;
   onRemoveStoryPress: () => void;
   onEditStoryPress: () => void;
   onClose: () => void;
 };
 
 export const EditAndDeleteBottomSheet: React.FC<EditAndDeleteBottomSheetTypes> =
-  ({ isVisible, onRemoveStoryPress, onEditStoryPress, onClose }) => {
+  ({
+    isVisible,
+    onRemoveStoryPress,
+    onEditStoryPress,
+    onClose,
+    editStoryTestID,
+    deleteStoryTestID,
+  }) => {
     return (
       <SystemBottomSheet isVisible={isVisible} onClose={() => onClose()}>
         <Box width="100%" marginLeft="m" marginRight="m">
-          <Box marginLeft="m" marginRight="m">
+          <Box marginLeft="m" marginRight="m" testID={editStoryTestID}>
             <Button
               title="Edit story"
               onPress={() => onEditStoryPress()}
@@ -23,7 +32,7 @@ export const EditAndDeleteBottomSheet: React.FC<EditAndDeleteBottomSheetTypes> =
               backgroundColor="backgroundContrastXLow"
             />
           </Box>
-          <Box marginLeft="m" marginRight="m">
+          <Box marginLeft="m" marginRight="m" testID={deleteStoryTestID}>
             <Button
               title="Delete story"
               onPress={() => onRemoveStoryPress()}
